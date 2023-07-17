c4d1eb914db5ca456b959948fae651748072dc95
Adityasc9 <adityasc16@gmail.com>
2023-07-17 13:42:19 +0400

    changed html title

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
 import "./cssFiles/NavBar.css";
 
 function NavBar() {
   return (
     <div className="text-center">
-      <a href="/spotilyzer" className="logo">
+      <a href="/" className="logo">
         Spotilyzer
       </a>
     </div>
